a2023214f46c82d65258381ee2c88361311b6425
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-01-26 16:24:40 -0500

    See #139.  Added the SYSTEM Resolver.

---
+++ terraform/setup-confluent-environment.tf
@@ -0,0 +1,21 @@
+resource "confluent_environment" "non_prod" {
+  display_name = "Non-Prod"
+
+  stream_governance {
+    package = "ESSENTIALS"
+  }
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "confluent_private_link_attachment" "non_prod" {
+  cloud        = "AWS"
+  region       = var.aws_region
+  display_name = "non-prod-aws-platt"
+  
+  environment {
+    id = confluent_environment.non_prod.id
+  }
+}
