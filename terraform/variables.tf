e6ede99824473d9a67d134b9ccd4352ed49d1df0
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-02-01 00:28:47 -0500

    See #160.  Code complete.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -120,7 +120,6 @@ variable "tgw_rt_id" {
 variable "vpn_vpc_id" {
   description = "VPN Client VPC ID - Private Hosted Zones will be associated with this VPC"
   type        = string
-  default     = "vpc-05946aab047008805"
 }
 
 variable "vpn_client_vpc_cidr" {
@@ -135,11 +134,7 @@ variable "vpn_vpc_cidr" {
 
 variable "vpn_client_vpc_rt_ids" {
   description = "List of route table IDs for VPN Client VPC"
-  type        = list(string)
-  default     = [
-    "rtb-03864e3b099d45c70",
-    "rtb-0a872eae8a88644af"
-  ]
+  type        = string
 }
 
 variable "dns_vpc_cidr" {
